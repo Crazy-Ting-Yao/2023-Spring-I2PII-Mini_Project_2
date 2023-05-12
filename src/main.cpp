29031f7e88fa2932245958577a1e7d9b64e21f60
瘋狂挺腰獸 <s111062109@m111.nthu.edu.tw>
2023-05-12 22:09:12 +0800

    Classify files

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+// [main.cpp]
+// this template is provided for the 2D tower defense game.
+// Program entry point.
+// Returns program exit code.
+#include "include/GameEngine.hpp"
+#include "include/LOG.hpp"
+#include "include/LoseScene.hpp"
+#include "include/PlayScene.hpp"
+#include "include/StartScene.hpp"
+#include "include/StageSelectScene.hpp"
+#include "include/WinScene.hpp"
+
+int main(int argc, char **argv) {
+    Engine::LOG::SetConfig(true);
+    Engine::GameEngine& game = Engine::GameEngine::GetInstance();
+    game.AddNewScene("stage-select", new StageSelectScene());
+    game.AddNewScene("play", new PlayScene());
+    game.AddNewScene("lose", new LoseScene());
+    game.AddNewScene("win", new WinScene());
+    game.AddNewScene("start-scene", new StartScene());
+    game.Start("start-scene", 60, 1600, 832);
+    return 0;
+}
